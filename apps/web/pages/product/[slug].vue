d841a369fb25015d07c5a6d4bbd97b3d3000350a
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-02-24 09:29:39 +0100

    fix: recommended products on product pages (#1075)

--- apps/web/pages/product/[slug].vue
+++ apps/web/pages/product/[slug].vue
@@ -30,9 +30,7 @@
         <p class="g-16 lg:g-18 mt-2">Weitere Produkte</p>
         <p class="g-40 lg:g-56 mb-6">Das könnte dir auch gefallen</p>
         <NuxtLazyHydrate when-visible>
-          <BlocksProductRecommendedProducts
-                                            :category-id="productGetters.getCategoryIds(product)[0]"
-                                            :text="{ title: '' }" />
+          <BlocksProductRecommendedProducts :text="{title: ''}" :category-id="productGetters.getCategoryIds(product)[0]" />
         </NuxtLazyHydrate>
       </section>
     </NarrowContainer>
