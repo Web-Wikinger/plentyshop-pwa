e63bd6da31ff7b0b0108ab1b3b365c4fc8b619a1
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-04-15 16:45:28 +0300

    feat: drag & drop blocks sorting (#1184)
    Co-authored-by: Kevin Stederoth <43753494+ksted@users.noreply.github.com>

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -8,56 +8,12 @@
 </template>
 
 <script lang="ts" setup>
-const { isClicked, clickedBlockIndex, isTablet, blockHasData, tabletEdit, changeBlockPosition } = useBlockManager();
-
 const { t } = useI18n();
-const { settingsIsDirty, closeDrawer } = useSiteConfiguration();
-
-const { data, getBlocks } = useCategoryTemplate();
-
-const dataIsEmpty = computed(() => data.value.length === 0);
-
-const { isEditingEnabled, disableActions } = useEditor();
-const { getRobots, setRobotForStaticPage } = useRobots();
-
 const { setPageMeta } = usePageMeta();
-
 const icon = 'home';
 setPageMeta(t('homepage.title'), icon);
 
-await getBlocks('index', 'immutable');
-
-onMounted(() => {
-  isEditingEnabled.value = false;
-  window.addEventListener('beforeunload', handleBeforeUnload);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('beforeunload', handleBeforeUnload);
-});
-
-const hasUnsavedChanges = () => {
-  return !isEditingEnabled.value && !settingsIsDirty.value;
-};
-
-const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-  if (hasUnsavedChanges()) return;
-  event.preventDefault();
-};
-onBeforeRouteLeave((to, from, next) => {
-  if (isEditingEnabled.value) {
-    const confirmation = window.confirm('You have unsaved changes. Are you sure you want to leave?');
-    if (confirmation) {
-      closeDrawer();
-      next();
-    } else {
-      next(false);
-    }
-  } else {
-    next();
-  }
-});
-
+const { getRobots, setRobotForStaticPage } = useRobots();
 getRobots();
 setRobotForStaticPage('Homepage');
 </script>
