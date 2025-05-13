85c2c8cdd3d4bfa429c5ad0ebbf5da52e57216ff
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2025-05-13 14:10:17 +0100

    [BulkAdd] Add coupon code to the bulk add

--- apps/web/plugins/bulkAdd.client.ts
+++ apps/web/plugins/bulkAdd.client.ts
@@ -4,12 +4,29 @@ export default defineNuxtPlugin(() => {
   const route  = useRoute()
   const router = useRouter()
   const { data: cart, addToCart, setCartItemQuantity, deleteCartItem } = useCart()
-
+  const { addCoupon, deleteCoupon, loading: couponLoading } = useCoupon()
   const isBlocking = useState('cartBlocking', () => false);
 
   onNuxtReady(async () => {                // fires after client init :contentReference[oaicite:1]{index=1}
     const queries = route.query
     let added = 0
+
+
+    // Handle coupon code if present
+    const couponCode = typeof queries.coupon === 'string' ? queries.coupon.trim() : ''
+    if (couponCode) {
+      isBlocking.value = true
+      try {
+        console.log(couponCode)
+        await addCoupon({couponCode})
+      } catch (err) {
+        console.error('Coupon apply failed', err)
+      }
+      added++
+    }
+    // Remove coupon from further processing
+    delete queries.coupon
+
     for (const [pid, rawQty] of Object.entries(queries)) {
       isBlocking.value = true;
       const productId = parseInt(pid, 10)
@@ -17,7 +34,7 @@ export default defineNuxtPlugin(() => {
     
       if (!isNaN(productId) && qty > 0) {
         const existing = cart.value?.items?.find(item => item.variationId === productId) ?? null;
-        const { data: product, fetchProduct } = useProduct(productId.toString());
+        const { data: product } = useProduct(productId.toString());
         if (existing) {
           if (qty > 0) {
             // update quantity
@@ -49,7 +66,6 @@ export default defineNuxtPlugin(() => {
     isBlocking.value = false;
   })
   const getCartItem = (product: Product) => {
-    const { data: cart } = useCart();
     const variationId = Number(productGetters.getVariationId(product));
   
     const cartItem = cart.value?.items?.find((item) => item.variationId === variationId);
