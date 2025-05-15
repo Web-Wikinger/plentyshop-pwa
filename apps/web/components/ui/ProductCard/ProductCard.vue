50b959551338411f070dbb0b8778c31f8965a634
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2025-05-15 12:49:36 +0100

    [ProductCard] Reload page on log in

--- apps/web/components/ui/ProductCard/ProductCard.vue
+++ apps/web/components/ui/ProductCard/ProductCard.vue
@@ -33,7 +33,7 @@
       <SfLink :tag="NuxtLink" :to="productPath" class="no-underline g-12 lg:g-16" variant="secondary" data-testid="productcard-name">
         {{ name }}
       </SfLink>
-      <p v-if="isAuthorized" class="g-12-m lg:g-16-m mb-2 mt-1">{{ getWeight(product) }}g ({{ n(getKgPrice(product), 'currency') }}/kg )</p>
+      <p class="g-12-m lg:g-16-m mb-2 mt-1">{{ getWeight(product) }}g ({{ n(getKgPrice(product), 'currency') }}/kg )</p>
 
       <!-- <div class="flex items-center gap-1" :class="{ 'mb-2': !productGetters.getShortDescription(product) }">
         <SfRating size="xs" :half-increment="true" :value="rating ?? 0" :max="5" />
@@ -50,12 +50,13 @@
       <LowestPrice :product="product" />
 
       <div class="flex items-center">
-        <div v-if="isAuthorized" class="flex flex-col-reverse items-start md:flex-row md:items-center">
+        <div v-if="isAuthorized && priceWithProperties" class="flex flex-col-reverse items-start md:flex-row md:items-center">
           <span class="block font-bold typography-text-sm" data-testid="product-card-vertical-price">
             <!-- <span v-if="!productGetters.canBeAddedToCartFromCategoryPage(product)" class="mr-1">
               {{ t('account.ordersAndReturns.orderDetails.priceFrom') }}
             </span> -->
             <Price :price="priceWithProperties" :crossed-price="crossedPrice" />
+
             <!-- <span class="g-16 lg:g-24" v-if="showNetPrices">{{ t('asterisk') }} </span> -->
           </span>
           <span v-if="crossedPrice" class="typography-text-sm text-neutral-500 line-through md:ml-3 g-16-m">
@@ -65,7 +66,7 @@
 
         <div class="ml-auto">
           <KelloggsAddToBasket
-              v-if="isAuthorized"
+              v-if="isAuthorized && priceWithProperties"
               :product-id="Number(productGetters.getId(product))"
               :available="productGetters.canBeAddedToCartFromCategoryPage(product)"
               @added-to-cart="handleAddToCartSuccess"
@@ -143,11 +144,12 @@ const getHeight = () => {
 const priceWithProperties = computed(() => {
   if (!isAuthorized.value) return 0;
 
-  return (
+  let price = (
     (productGetters.getSpecialOffer(product) ||
       productGetters.getGraduatedPriceByQuantity(product, 1)?.unitPrice.value ||
       0) + getPropertiesPrice(product)
   );
+  return price > 1000 ? 0 : price
 });
 
 // Handle success of adding to cart (can be used to trigger notifications, etc.)
@@ -172,4 +174,14 @@ const getKgPrice = (product: Product) => {
 }
 
 const NuxtLink = resolveComponent('NuxtLink');
+
+
+watch(isAuthorized, (newValue: Boolean) => {
+  if (newValue) {
+    window.location.reload();
+
+  } else {
+
+  }
+});
 </script>
