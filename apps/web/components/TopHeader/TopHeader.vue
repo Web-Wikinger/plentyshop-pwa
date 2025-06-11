98d35786319e66dce3befc1d44053efc7f9bcc61
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2025-06-11 11:23:37 +0100

    [TopHeader] Make ® smaller

--- apps/web/components/TopHeader/TopHeader.vue
+++ apps/web/components/TopHeader/TopHeader.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="relative left-0 w-full min-h-[54px] bg-[#2c2c2c] text-white p-2 text-center md:flex md:justify-center md:items-center z-50">
-  <span class="block text-[12px] lg:text-[21px] font-bold">Der Onlineshop für Kellogg's® Businesspartner</span>
+  <span class="block text-[12px] lg:text-[21px] font-bold">Der Onlineshop für Kellogg's<span class='text-lg align-super'>®</span> Businesspartner</span>
   <span class="block md:inline md:ml-4 ">
     <NuxtLink to="/kelloggs/register" class="text-[#d0033d] text-[12px] lg:text-[21px] text-bold">
       Jetzt Registrieren
