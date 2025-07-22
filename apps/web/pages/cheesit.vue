a3101960f237b829443f0773e0b2e5caf010b729
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2025-07-22 12:05:04 +0100

    CheezIt LP in PWA B2B

--- apps/web/pages/cheesit.vue
+++ apps/web/pages/cheesit.vue
@@ -3,26 +3,26 @@
     <LandingPageHeader 
       title="Käsecracker"
       subtitle="Vom Kultprodukt zum Sortiments-Booster"
-      description="<p>Cheez-It ist in den USA längst ein Klassiker: Die ikonischen Käse-Snacks auf Weizenbasis sind dort in Millionen Haushalten zu finden – als knuspriger Begleiter für unterwegs, auf der Couch oder im Büro.</p>
-                  <p>In den USA jahrzehntelang als Käsecracker bekannt, gehört Cheez-It mittlerweile zu den bekanntesten Marken im Segment gebackener Käse-Snacks. Mit einem Marktanteil von rund 67 % im US-Segment für Convenience-Cracker (Stand: 2023) führt Cheez-It dort die Kategorie an – ein Beleg für die Markenstärke und breite Verbraucherakzeptanz. *</p>
-                  <p>Jetzt kommt der knusprig gebackene Snack endlich auch nach Deutschland – und sorgt für frischen Schwung im Snackregal.</p>"
+      description="Cheez-It ist in den USA längst ein Klassiker: Die ikonischen Käse-Snacks auf Weizenbasis sind dort in Millionen Haushalten zu finden – als knuspriger Begleiter für unterwegs, auf der Couch oder im Büro.<br>
+                  In den USA jahrzehntelang als Käsecracker bekannt, gehört Cheez-It mittlerweile zu den bekanntesten Marken im Segment gebackener Käse-Snacks. Mit einem Marktanteil von rund 67 % im US-Segment für Convenience-Cracker (Stand: 2023) führt Cheez-It dort die Kategorie an – ein Beleg für die Markenstärke und breite Verbraucherakzeptanz. *<br>
+                  Jetzt kommt der knusprig gebackene Snack endlich auch nach Deutschland – und sorgt für frischen Schwung im Snackregal."
 
     />
     <img class="mt-6" src="/images/cheesit/cheesitlandingpage.png" >
 
     <LandingPagePresentation imageSrc="/images/cheesit/1.png" 
         Title="Jetzt neu: Cheez-It Snacks für den Handel"
-        Description="<p>Mit dem Deutschland-Launch zieht die beliebte US-Snackmarke in deine Regale ein. Cheez-It Snap’d überzeugt mit intensivem Cheddar-Geschmack, knuspriger Textur und sofortiger Verzehrbereitschaft – ideal für alle, die auf der Suche nach einem Impulsprodukt mit Wiedererkennungswert sind.</p>
-                    <p>Was in den USA als Käsecracker mit Kultstatus begann, sorgt jetzt als knusprig gebackener Käse-Snack für Aufmerksamkeit – ohne Kühlaufwand, aber mit maximaler Wirkung auf kleiner Fläche.</p>" 
+        Description="Mit dem Deutschland-Launch zieht die beliebte US-Snackmarke in deine Regale ein. Cheez-It Snap’d überzeugt mit intensivem Cheddar-Geschmack, knuspriger Textur und sofortiger Verzehrbereitschaft – ideal für alle, die auf der Suche nach einem Impulsprodukt mit Wiedererkennungswert sind.<br>
+                    Was in den USA als Käsecracker mit Kultstatus begann, sorgt jetzt als knusprig gebackener Käse-Snack für Aufmerksamkeit – ohne Kühlaufwand, aber mit maximaler Wirkung auf kleiner Fläche." 
         noButton
         />
     <LandingPagePresentation imageLeft :bgColor="'bg-gray-100'" imageSrc="/images/cheesit/2.png" 
         Title="Cheez-It Snap’d – Auffällig im Design, praktisch im Einsatz" 
-        Description="<p>Cheez-It ist mehr als nur ein Snack – es ist eine Marke mit Geschichte und Charakter. Das auffällige Packungsdesign, der ikonische US-Look und der intensive Cheddar-Geschmack machen Cheez-It Snap’d zum echten Hingucker im Regal. Und das Beste:</p>
-                    <p>✔ Kein Kühlaufwand</p>
-                    <p>✔ Sofort snackbereit für unterwegs</p>
-                    <p>✔ Ideal für Shops mit hoher Kundenfrequenz</p>
-                    <p>✔ Bekannt aus den USA</p>" 
+        Description="Cheez-It ist mehr als nur ein Snack – es ist eine Marke mit Geschichte und Charakter. Das auffällige Packungsdesign, der ikonische US-Look und der intensive Cheddar-Geschmack machen Cheez-It Snap’d zum echten Hingucker im Regal. Und das Beste:<br>
+                    ✔ Kein Kühlaufwand<br>
+                    ✔ Sofort snackbereit für unterwegs<br>
+                    ✔ Ideal für Shops mit hoher Kundenfrequenz<br>
+                    ✔ Bekannt aus den USA" 
         noButton
         
         />
@@ -30,11 +30,11 @@
         
     <LandingPagePresentation imageSrc="/images/cheesit/3.png" 
       Title="Sortiment clever erweitern – jetzt bestellen" 
-      Description="<p>Du willst dein Angebot auffrischen, neue Zielgruppen erreichen oder mehr Vielfalt in den Impulsbereich bringen? Dann ist Cheez-It Snap’d eine spannende Ergänzung. Über unseren B2B-Shop kannst du die Käse-Snacks ganz unkompliziert ordern – mit klarem Produktprofil und attraktiver Regalwirkung.</p>
-                    <p>Ideal für:</p>
-                    <p>✔ Impulsbereiche mit schneller Drehung</p>
-                    <p>✔ trendaffine Kundschaft</p>
-                    <p>✔ mobile Verkaufsflächen und To-go-Zonen</p>"
+      Description="Du willst dein Angebot auffrischen, neue Zielgruppen erreichen oder mehr Vielfalt in den Impulsbereich bringen? Dann ist Cheez-It Snap’d eine spannende Ergänzung. Über unseren B2B-Shop kannst du die Käse-Snacks ganz unkompliziert ordern – mit klarem Produktprofil und attraktiver Regalwirkung.<br>
+                    Ideal für:<br>
+                    ✔ Impulsbereiche mit schneller Drehung<br>
+                    ✔ trendaffine Kundschaft<br>
+                    ✔ mobile Verkaufsflächen und To-go-Zonen"
       noButton/>
 
 
@@ -43,8 +43,8 @@
 
     <LandingPagePresentation imageLeft :bgColor="'bg-gray-100'" imageSrc="/images/cheesit/4.png" 
       Title="Jetzt Sortiment stärken – mit Cheez-It Snap’d im Online Shop" 
-      Description="<p>Ob für den Impulsbereich, das Snackregal oder mobile Verkaufsflächen – Cheez-It Snap’d passt in moderne Sortimente. Der knusprig gebackene Käse-Snack ist unkompliziert in der Handhabung, lange haltbar und sorgt dank auffälligem Design für schnelle Kaufimpulse – auch auf kleiner Fläche.</p>
-                  <p>Du willst direkt loslegen? Dann melde dich jetzt für unseren B2B-Newsletter an und erfahre sofort, sobald Cheez-It Snap’d verfügbar ist.</p>"
+      Description="Ob für den Impulsbereich, das Snackregal oder mobile Verkaufsflächen – Cheez-It Snap’d passt in moderne Sortimente. Der knusprig gebackene Käse-Snack ist unkompliziert in der Handhabung, lange haltbar und sorgt dank auffälligem Design für schnelle Kaufimpulse – auch auf kleiner Fläche.<br>
+                  Du willst direkt loslegen? Dann melde dich jetzt für unseren B2B-Newsletter an und erfahre sofort, sobald Cheez-It Snap’d verfügbar ist."
       noButton
       />
     
